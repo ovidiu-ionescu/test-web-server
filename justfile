b46299a4547d62c0930fae19816a056fba11a2b7
Ovidiu Ionescu <ovidiu@ionescu.net>
2024-11-07 12:45:59 +0100

    Command line parameters to override default values for address and directory

---
+++ justfile
@@ -0,0 +1,2 @@
+run:
+  RUST_LOG=info cargo run --  -a "127.0.0.1:3000"
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{convert::Infallible, net::SocketAddr};
+use clap::Parser;
+use std::{convert::Infallible, sync::Arc};
 
 use http_body_util::Full;
 use hyper::{
@@ -8,37 +9,59 @@ use hyper::{
   Request, Response,
 };
 use hyper_util::rt::TokioIo;
+use log::{debug, info};
 use mime_guess::MimeGuess;
 use tokio::net::TcpListener;
-use log::debug;
+
+#[derive(Parser)]
+struct Args {
+  #[arg(short, long, default_value = "127.0.0.1:8080", help = "IP address to bind to")]
+  address: String,
+  #[arg(short, long, default_value = "Public", help = "Directory to serve files from")]
+  dir: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-  let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+  env_logger::init();
+  let args = Args::parse();
+  let addr = args.address.parse::<std::net::SocketAddr>()?;
   let listener = TcpListener::bind(&addr).await?;
+  info!("Listening on: 「{}」, serving files from 「{}」", addr, &args.dir);
+
+  let config = Arc::new(args.dir.clone());
 
   loop {
     let (stream, _) = listener.accept().await?;
     let io = TokioIo::new(stream);
 
+    let config = Arc::clone(&config);
     tokio::task::spawn(async move {
-      if let Err(err) = http1::Builder::new().serve_connection(io, service_fn(hello)).await {
+      if let Err(err) = http1::Builder::new()
+        .serve_connection(
+          io,
+          service_fn(move |req| {
+            let acfg = Arc::clone(&config);
+            async move { serve_file(req, Arc::clone(&acfg)).await }
+          }),
+        )
+        .await
+      {
         eprintln!("Error serving connection: {:?}", err);
       }
     });
   }
 }
 
-const DIR: &str = "Public";
-
-async fn hello(req: Request<Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+async fn serve_file(req: Request<Incoming>, adir: Arc<String>) -> Result<Response<Full<Bytes>>, Infallible> {
   let path = req.uri().path();
   // check if path contains ".." to prevent directory traversal
   if path.contains("..") {
     eprintln!("Invalid path: {}", path);
     return Ok(Response::builder().status(400).body(Full::new(Bytes::from("400 Bad Request"))).unwrap());
   }
-  let file_path = format!("{}/{}", DIR, path);
+  let directory = &adir;
+  let file_path = format!("{}/{}", directory, path);
   let file = match tokio::fs::read(file_path).await {
     Ok(file) => file,
     Err(_) => {
@@ -47,8 +70,7 @@ async fn hello(req: Request<Incoming>) -> Result<Response<Full<Bytes>>, Infallib
     }
   };
   let length = file.len();
-  //let mime = "text/html";
-  let mime = MimeGuess::from_path(&path).first_or_text_plain();
+  let mime = MimeGuess::from_path(path).first_or_text_plain();
   debug!("{}: {:?}", path, mime);
 
   // set the length of the file in the response header
